705a94df2a7811bf585d9d566f3c204e93ccb077
EiPhyu15 <thineiphyu1561998@gmail.com>
2025-05-29 15:24:57 +0800

    updated rolesadmin

--- HotelReservationSystemProject/obj/Debug/net8.0/HotelReservationSystemProject.AssemblyInfo.cs
+++ HotelReservationSystemProject/obj/Debug/net8.0/HotelReservationSystemProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelReservationSystemProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c251c733bf3900ec2711ff1c219d457dc2414d46")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelReservationSystemProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelReservationSystemProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
